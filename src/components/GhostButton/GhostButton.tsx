88754e3cee4928d54836a8343d8a5502d6f7a1d6
Chaokai Lai <chaokai.lai@gmail.com>
2021-03-14 04:48:16 +1000

    home header done

--- src/components/GhostButton/GhostButton.tsx
+++ src/components/GhostButton/GhostButton.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import './GhostButton.scss';
 import { Button } from '@material-ui/core';
 
-const GhostButton: React.FC = () => {
+interface ghostButtonProps {
+  title: string;
+  width: number;
+  onClick: any;
+}
+
+const GhostButton: React.FC<ghostButtonProps> = (props) => {
+  const { title, width, onClick } = props;
   return (
-    <div>
-      <Button className="ghost-btn">resume</Button>
+    <div className="ghost-btn" onClick={onClick}>
+      <span className="ghost-btn-mask" style={{ width: `${width}px` }}>
+        {title}
+      </span>
+      <Button>{title}</Button>
     </div>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './HomePage.scss';
 import { Container, Typography, Hidden, Grid } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import photo from '../../assets/img/home-page-photo.jpg';
 import photoMask from '../../assets/img/home-page-photo-mask.png';
 import GhostButton from '../../components/GhostButton/GhostButton';
+import Typed from 'react-typed';
+
+// const typed = new Typed('.home__header__intro-content__text__typed-text', {
+//   strings: ['Web developer.', 'Designer.', 'Photographer.'],
+//   typeSpeed: 100,
+//   backSpeed: 70,
+//   loop: true,
+//   backDelay: 3000,
+// });
+
+// var typed = new Typed('.element', options);
 
 const HomePage: React.FC = () => {
+  const history = useHistory();
+  const handleOnclick = () => {
+    history.push('/resume');
+  };
+
   return (
     <main className="home-page">
       <section className="intro">
@@ -16,25 +32,32 @@ const HomePage: React.FC = () => {
         </div>
         <Container>
           <div className="intro__content">
-            <Grid className="intro__content-container" container alignItems="center" xs={12}>
+            <Grid className="intro__content-container" container alignItems="center">
               <Hidden xsDown>
                 <Grid item sm={1} md={3} lg={4} />
               </Hidden>
               <Grid className="intro__content-item" container justify="flex-end" item xs={12} sm={11} md={9} lg={7}>
                 <Grid className="intro__content-text" item container xs={12}>
-                  <Grid item>
+                  <Grid item xs={12} md="auto">
                     <p>I'm a</p>
                   </Grid>
-                  <Grid item>
-                    <p>web developer.</p>
+                  <Grid item className="element">
+                    <Typed
+                      strings={['Web developer.', 'Designer.', 'Photographer.']}
+                      typeSpeed={100}
+                      backSpeed={70}
+                      className="intro-typed"
+                      loop={true}
+                      backDelay={3000}
+                    />
                   </Grid>
                 </Grid>
-                <Grid item container alignItems="center" xs={12}>
-                  <Grid item>
+                <Grid className="intro-name-grid" item container alignItems="center" xs={12}>
+                  <Grid item xs={12} sm="auto">
                     <span className="intro-name">Chaokai Lai</span>
                   </Grid>
                   <Grid item>
-                    <GhostButton />
+                    <GhostButton title="resume" width={122} onClick={handleOnclick} />
                   </Grid>
                 </Grid>
               </Grid>
